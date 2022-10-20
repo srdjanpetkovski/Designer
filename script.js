56d0f108e43e791e4d45936c6dc72d441e29d4cd
srdjanpetkovski <srdjanpetkovski@gmail.com>
2022-10-20 22:26:32 +0200

    added some button features

---
+++ script.js
@@ -0,0 +1,26 @@
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+
+const showModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+    btnsShowModal[i].addEventListener('click', showModal);
+}
+
+modal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+})
